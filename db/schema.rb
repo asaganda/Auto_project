2dd77b3817d9821545eef74efa262154c0bab8fd
Andrew <Asaganda@gmail.com>
2015-08-21 15:13:38 -0400

    Start car model, built simple relationship between user and car, simple table for car and its fields in database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150820150519) do
+ActiveRecord::Schema.define(version: 20150821185214) do
+
+  create_table "cars", force: :cascade do |t|
+    t.string   "make"
+    t.string   "model"
+    t.string   "year"
+    t.string   "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false

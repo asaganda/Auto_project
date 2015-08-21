45ff5981577607132bda551e233cf450a9050417
Andrew <Asaganda@gmail.com>
2015-08-21 12:11:25 -0400

    clean up what i don't need

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,8 +4,4 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resources)
     special_path
   end
-
-  # def after_sign_in_path_for(resources)
-  #   after_sign_in_path
-  # end
 end

00d574ee19ea97eb0503219c67394099d85c5651
Andrew <Asaganda@gmail.com>
2015-08-22 00:29:36 -0400

    took out code i didn't need. another controller is handling it for me

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def after_sign_up_path_for(resource)
-    special_path
-  end
-
+  
   def after_sign_in_path_for(resources)
     summary_page_path
   end

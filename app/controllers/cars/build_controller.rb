82501ac2479da9f17d444f910a527873e7e98e1b
Andrew <Asaganda@gmail.com>
2015-08-24 12:14:08 -0400

    haven't figured out how to build a car, keeping this branch for late. this branch branched off product_build_car

--- app/controllers/cars/build_controller.rb
+++ app/controllers/cars/build_controller.rb
@@ -1,20 +1,31 @@
 class Cars::BuildController < ApplicationController
-  include Wicked::Wizard
 
+  # skip_before_filter :setup_wizard, only: :show
+  include Wicked::Wizard
+  # skip_before_filter :setup_wizard, only: :show
   steps :car_build, :dealer_contact
 
+
+  # def car_build
+  #   @car = Car.find(params[:car_id])
+  #   render_wizard
+
+  # end 
   def show
+    # binding.pry
     @car = Car.find(params[:car_id])
     render_wizard
   end
 
   def update
+    binding.pry
     @car = Car.find(params[:car_id])
     @car.update_attributes(car_params)
     render_wizard @car
   end
 
   def create
+    binding.pry
     @car = Car.create
     redirect_to wizard_path(steps.first, :car_id => @car.id)
   end
--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,13 +2,20 @@ class CarsController < ApplicationController
 
   def create
 
-    @car = Car.new(car_params)
-    @car.save 
-    redirect_to car_build_path(car_id: @car.id, id: :details)
+    @car = Car.new
+    @car.save
+    redirect_to car_build_path(car_id: @car.id, id: @car.id)
 
     # @car = Car.new(car_params)
     # @car.save
     # redirect_to wizard_path
+  end
+
+  private
+
+  def car_params
+
+    params.require(:car).permit(:make, :model, :year, :color)
   end
 
 end

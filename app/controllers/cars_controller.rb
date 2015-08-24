8b008344b6cbab2e8bf03cf4afd53454d1af81b9
Andrew <Asaganda@gmail.com>
2015-08-23 23:19:20 -0400

    comment out cars controoller because a new car wasn't being created

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,12 +1,12 @@
 class CarsController < ApplicationController
 
-  def new
-    @car = Car.new
-  end
+  # def new
+  #   @car = Car.new
+  # end
 
-  def create
-    @car = Car.new(car_params)
-    @car.save
-    redirect_to wizard_path
-  end
+  # def create
+  #   @car = Car.new(car_params)
+  #   @car.save
+  #   redirect_to wizard_path
+  # end
 end

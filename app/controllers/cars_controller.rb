e40c85c858728263e895672d68634c5370a5b896
Andrew <Asaganda@gmail.com>
2015-08-24 19:13:41 -0400

    changed redirect route path for regular after create. goes to summary_page_path now, must establish that path

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -20,7 +20,7 @@ class CarsController < ApplicationController
       redirect_to after_signup_path(:dealer_contact)
     else
       #change to summary page or something
-      redirect_to root_path
+      redirect_to summary_page_path
     end
   end
 
@@ -36,6 +36,6 @@ class CarsController < ApplicationController
   private
 
   def car_params
-    params.require(:car).permit(:make, :model, :year, :color)
+    params.require(:car).permit(:make, :model, :year, :color
   end
 end

47b2cb40c3e312886e3d423efd0793154f065e77
Andrew <Asaganda@gmail.com>
2015-08-25 13:41:30 -0400

    Start psuedo-coding/commenting the cars controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,25 +1,33 @@
 class CarsController < ApplicationController
   
+  # shows all the user's cars that they have added to the database
   def summary_page
+    # All the users' cars
     @car = Car.all
   end
 
+  # making a new car instance
   def new
     @car = Car.new
   end
 
+  # creating a new car instance
   def create
     @car = Car.create(car_params)
 
     #this will break unless car has user_id
-    # binding.pry
+    # assigning the current_user's id to the car they're making
     @car.user_id = current_user.id
+    # saving the car instance that has just been created
     @car.save
 
+    # this corresponds to users that have just signed up
+    # [:onboarding] tag is in the car_build.html.erb ONLY for new users
+    # once user creates their first car, they're redirected to dealer_contact
     if params[:onboarding].present?
       redirect_to after_signup_path(:dealer_contact)
     else
-      #change to summary page or something
+      # WAITING FOR COMMENT
       redirect_to summary_page_path
     end
   end
